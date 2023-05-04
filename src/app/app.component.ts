2f18fa7520d732cb69f7a9a3cf431c416faa5307
rmalegr <rodrigomalegre@gmail.com>
2023-05-04 01:34:01 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ngrx';
+  nombrePersona :string = 'Rodrigo'
+
+  imprimirNombre(nombre:string):void{
+    if (nombre != undefined && nombre != null){
+        const alumno = {
+          name: nombre,
+          dni: '30790171',
+        };
+       console.log(alumno.name.toUpperCase());
+    }
+   
+  }
 }
